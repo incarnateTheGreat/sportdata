383e2b8c2a014764344caf79df1d79fb44844925
Garry T <incarnate@rogers.com>
2018-03-02 11:53:20 -0500

    Wrapped Header with an HOC to support withRouter via react-router.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { withRouter } from 'react-router';
 
-export default class Header extends Component {
+class Header extends Component {
+	constructor(props) {
+		super()
+
+		this.navigateTo = this.navigateTo.bind(this);
+	}
+	navigateTo(path) {
+		this.props.history.push(path);
+	}
+
   render() {
 		return (
 			<header>
 				<h1>Sport Data</h1>
 				<nav>
 					<ul>
-						<li>Home</li>
-						<li>Standings</li>
+						<li onClick={() => this.navigateTo('/')}>Home</li>
+						<li onClick={() => this.navigateTo('/standings')}>Standings</li>
 					</ul>
 				</nav>
 			</header>
 		)
   }
 }
+
+export default withRouter(Header);
