8cbd49c0434a953dcd630a5511ccb75531f7a389
Garry T <incarnate@rogers.com>
2018-03-06 12:42:03 -0500

    Set up Events view.

--- src/components/FixtureData.js
+++ src/components/FixtureData.js
@@ -6,17 +6,12 @@ export default class FixtureData extends Component {
 
 		return (
 			<div id={id} className='fixture-data'>
-				<div className='fixture-data__header'>
-					<span className='fixture-data__header__team'>{fixture.match_hometeam_name}</span>
-					<span className='fixture-data__score'>{fixture.match_hometeam_score} - {fixture.match_awayteam_score}</span>
-					<span className='fixture-data__header__team'>{fixture.match_awayteam_name}</span>
-				</div>
 				<div className='fixture-data__events'>
 					{fixture.goalscorer.map((e, i) =>
 						<div className='fixture-data__events__row' key={i}>
-							<span className='fixture-data__events__row__event'>{e.home_scorer !== '' ? e.home_scorer : e.away_scorer}</span>
-							<span className='fixture-data__events__row__time'>{e.time}</span>
-							<span className='fixture-data__events__row__event'>{e.home_scorer !== '' ? e.home_scorer : e.away_scorer}</span>
+							<span className='fixture-data__events__row__event'>{e.home_scorer !== '' ? e.home_scorer : ''}</span>
+							<span className='fixture-data__events__row__event'>{e.time}</span>
+							<span className='fixture-data__events__row__event'>{e.away_scorer !== '' ? e.away_scorer : ''}</span>
 						</div>
 					)}
 				</div>
