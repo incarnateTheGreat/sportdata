7ae9db97081dfd84dd8ff5e1a832d7277f4427b7
Garry T <incarnate@rogers.com>
2018-03-26 15:05:06 -0400

    Added Last 3 Meetings.

--- src/components/H2HData.js
+++ src/components/H2HData.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import { ScaleLoader } from 'react-spinners';
 
-const statistic = (e, i) => {
+const h2h = (e, i) => {
 	return (
-		<div className='fixture-data__statistics__row' key={i}>
-			<span className='fixture-data__statistics__row__event'>{e['home']}</span>
-			<span className='fixture-data__statistics__row__event --type'>{e['type']}</span>
-			<span className='fixture-data__statistics__row__event'>{e['away']}</span>
+		<div className='h2h-data__first-v-second__row' key={i}>
+			<span className='h2h-data__first-v-second__date'>{moment(e['match_date']).format('MMM D, YYYY')}</span>
+      <div className='h2h-data__first-v-second__data'>
+        <span className='h2h-data__first-v-second__match-info'>{e['match_hometeam_name']}</span>
+        <span className='h2h-data__first-v-second__match-info --score'>
+          {e['match_hometeam_score']} - {e['match_awayteam_score']}
+        </span>
+        <span className='h2h-data__first-v-second__match-info'>{e['match_awayteam_name']}</span>
+      </div>
 		</div>
 	)
 };
@@ -18,9 +24,10 @@ export default class H2HData extends Component {
             data } = this.props;
 
     if (data) {
-      const { last_three_matches,
-              firstTeam_lastResults,
-              secondTeam_lastResults } = data;
+      console.log(data);
+      // firstTeam_VS_secondTeam
+      // firstTeam_lastResults
+      // secondTeam_lastResults
     }
 
 		return (
@@ -31,8 +38,13 @@ export default class H2HData extends Component {
 						loading={true}
 					/>
 				</div>
-        {data && (
-          <div>Head-to-Head Stats</div>
+        {data && data.firstTeam_VS_secondTeam.length > 0 && (
+          <div className='h2h-data__first-v-second'>
+            <h3 className='h2h-data__title'>Last 3 Meetings</h3>
+            {data.firstTeam_VS_secondTeam.map((e, i) =>
+              h2h(e, i)
+            )}
+          </div>
         )}
 			</div>
 		);
