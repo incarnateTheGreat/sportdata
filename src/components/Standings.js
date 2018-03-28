fd9d225b83d6e2c664067df60274fc8cb7d077d9
Garry T <incarnate@rogers.com>
2018-03-27 20:54:08 -0400

    Refined the Standings Sort.

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -22,9 +22,10 @@ class Standings extends Component {
 
 		// Return Standings Data.
 		this.getData(url).then(data => {
-			const standings = data.data;
+			let standings = data.data;
 
-			console.log(data.data);
+			// Add the Goal Difference pairing.
+			standings.forEach((e) => e['overall_league_GD'] = (e['overall_league_GF'] - e['overall_league_GA']).toString());
 
 			// Sort Standings by Overall Standings ASC (default).
 			standings.sort((a,b) => a.overall_league_position - b.overall_league_position)
@@ -36,25 +37,41 @@ class Standings extends Component {
 	sortTable(column) {
 		const columnID = column.target.id;
 		let standings = null,
-				sortDirectin = '';
-
-		console.log(columnID);
+				sortDirectin = '',
+				classList = column.target.classList,
+				arrows = document.getElementsByClassName('standings__arrow');
 
 		if (this.state.standings) {
 			standings = this.state.standings;
 
-			if (this.state.sortDirectin === 'ASC') {
-				standings.sort((a, b) => b[columnID] - a[columnID]);
-				sortDirectin = 'DESC';
+			// Clear all the active Arrows.
+			for (let i = 0; i < arrows.length; i++) arrows[i].innerHTML = '';
+
+			// Sort Alphabetically.
+			if (classList.contains('--non-numeric')) {
+				if (this.state.sortDirectin === 'ASC') {
+					standings.sort((a,b) => a[columnID].localeCompare(b[columnID]));
+					sortDirectin = 'DESC';
+				} else {
+					standings.sort((a,b) => b[columnID].localeCompare(a[columnID]));
+					sortDirectin = 'ASC';
+				}
 			} else {
-				standings.sort((a, b) => a[columnID] - b[columnID]);
-				sortDirectin = 'ASC';
+				// Sort Numerically.
+				if (this.state.sortDirectin === 'ASC') {
+					standings.sort((a, b) => b[columnID] - a[columnID]);
+					sortDirectin = 'DESC';
+				} else {
+					standings.sort((a, b) => a[columnID] - b[columnID]);
+					sortDirectin = 'ASC';
+				}
 			}
 
+			// Find the arrow selector that resides under the clicked table header and assign it.
+			let arrow = document.getElementById(columnID).querySelector('.standings__arrow');
+			arrow.innerHTML = (sortDirectin === 'ASC' ? '&#x25B2;' : '&#x25BC;');
 
-			this.setState({ standings, sortDirectin }, () => {
-				console.log(this.state);
-			});
+			this.setState({ standings, sortDirectin });
 		}
 	}
 
@@ -71,15 +88,37 @@ class Standings extends Component {
 							<thead>
 								<tr>
 									<th width='5%' id='overall_league_position' onClick={(e) => this.sortTable(e)}>
-										# <span className='standings__arrow'></span>
+										<span className='standings__label'>#</span>
+										<span className='standings__arrow'></span>
 									</th>
-									<th width='45%' id='team_name' className='--non-numeric' onClick={(e) => this.sortTable(e)}>Team</th>
-									<th width='5%' id='overall_league_GF' onClick={(e) => this.sortTable(e)}>MP</th>
-									<th width='5%' id='overall_league_W' onClick={(e) => this.sortTable(e)}>W</th>
-									<th width='5%' id='overall_league_D' onClick={(e) => this.sortTable(e)}>D</th>
-									<th width='5%' id='overall_league_L' onClick={(e) => this.sortTable(e)}>L</th>
-									<th width='5%' id='overall_league_GoalDifference' onClick={(e) => this.sortTable(e)}>G</th>
-									<th width='5%' id='overall_league_PTS' onClick={(e) => this.sortTable(e)}>Pts</th>
+									<th width='45%' id='team_name' className='--non-numeric' onClick={(e) => this.sortTable(e)}>
+										<span className='standings__label'>Team</span>
+										<span className='standings__arrow'></span>
+									</th>
+									<th width='5%' id='overall_league_GF' onClick={(e) => this.sortTable(e)}>
+										<span className='standings__label'>MP</span>
+										<span className='standings__arrow'></span>
+									</th>
+									<th width='5%' id='overall_league_W' onClick={(e) => this.sortTable(e)}>
+										<span className='standings__label'>W</span>
+										<span className='standings__arrow'></span>
+									</th>
+									<th width='5%' id='overall_league_D' onClick={(e) => this.sortTable(e)}>
+										<span className='standings__label'>D</span>
+										<span className='standings__arrow'></span>
+									</th>
+									<th width='5%' id='overall_league_L' onClick={(e) => this.sortTable(e)}>
+										<span className='standings__label'>L</span>
+										<span className='standings__arrow'></span>
+									</th>
+									<th width='5%' id='overall_league_GD' onClick={(e) => this.sortTable(e)}>
+										<span className='standings__label'>G</span>
+										<span className='standings__arrow'></span>
+									</th>
+									<th width='5%' id='overall_league_PTS' onClick={(e) => this.sortTable(e)}>
+										<span className='standings__label'>PTS</span>
+										<span className='standings__arrow'></span>
+									</th>
 								</tr>
 							</thead>
 							<tbody>
@@ -91,7 +130,7 @@ class Standings extends Component {
 										<td>{s.overall_league_W}</td>
 										<td>{s.overall_league_D}</td>
 										<td>{s.overall_league_L}</td>
-										<td>{s.overall_league_GF}:{s.overall_league_GA}</td>
+										<td className='--goal-difference'>{s.overall_league_GF}:{s.overall_league_GA}</td>
 										<td>{s.overall_league_PTS}</td>
 									</tr>
 								)}
